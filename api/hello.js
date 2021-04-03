44bd1f1442dba23eb8b778414454b22e8e46f6fb
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-03 15:10:22 +0800

    test?

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,8 @@
 
 export default async (req, res) => {
   const fetch = require("node-fetch");
+  const ogs = require("open-graph-scraper");
+  var upsidedown = require("upsidedown");
   let wildcard = req.headers.host.split(".")[0];
   console.log(wildcard);
   const { host } = req.headers;
@@ -63,25 +65,29 @@ export default async (req, res) => {
       .catch(() =>
         res.status(500).send("Encountered error serving profile page")
       );
-    console.log(
-      html
-        .replace(
-          "<head>",
-          "<head> <style>body{transform: rotate(180deg)}</style>"
-        )
-        .replace('content="Hack Club"', 'content="Australia Hack Club"')
-    );
-    res.send(
-      html
-        .replace(
-          `<script type="application/ld+json">{"@context":"http://schema.org","@type":"Organization","name":"Hack Club","url":"https://hackclub.com/","logo":"https://hackclub.com/social.png","sameAs":["https://twitter.com/hackclub","https://github.com/hackclub","https://www.instagram.com/starthackclub","https://www.facebook.com/Hack-Club-741805665870458","https://www.youtube.com/channel/UCQzO0jpcRkP-9eWKMpJyB0w"],"contactPoint":[{"@type":"ContactPoint","email":"team@hackclub.com","contactType":"customer support","url":"https://hackclub.com/"}]}</script>`,
-          ""
-        )
-        .replace(
-          "<head>",
-          "<head> <style>body{transform: rotate(180deg)}</style>"
-        )
-        .replace('"Don’t run your coding club alone – Hack Club"', 'content="Australia Hack Club"')
-    );
+    const options = {
+      url: `https://${
+        wildcard == "australia" ||
+        wildcard == "localhost:3000" ||
+        wildcard.includes("localhost")
+          ? ""
+          : `${wildcard}.`
+      }hackclub.com/${
+        typeof req.query.path == "undefined" ? "" : req.query.path
+      }`,
+    };
+    ogs(options, (error, results, response) => {
+      console.log("error:", error); // This is returns true or false. True if there was a error. The error it self is inside the results object.
+      console.log("results:", results); // This contains all of the Open Graph results
+      res.send(
+        html
+          .replace(
+            "<head>",
+            "<head> <style>body{transform: rotate(180deg)}</style>"
+          )
+          .replace(new RegExp(results.ogSiteName, 'g'), upsidedown(results.ogSiteName))
+          .replace(new RegExp(results.ogTitle, 'g'), upsidedown(results.ogTitle))
+      );
+    });
   }
 };
