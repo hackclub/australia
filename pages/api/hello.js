2b3bf8b790e285181679614802239ad056480283
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-03 13:21:08 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,13 +5,19 @@ export default async (req, res) => {
   let wildcard = req.headers.host.split(".")[0];
 
   const { host } = req.headers;
-  let html = await fetch(
-    `https://${wildcard == "australia" ? "" : `${wildcard}.`}hackclub.com/${req.query.path}`
-  )
-    .then((r) => r.text())
-    .catch(() =>
-      res.status(500).send("Encountered error serving profile page")
-    );
-  console.log(wildcard);
-  res.send(html);
+  if(req.query.path.inculdes('.')){
+    res.redirect(`https://${wildcard == "australia" ? "" : `${wildcard}.`}hackclub.com/${req.query.path}`)
+  }
+  else{
+    let html = await fetch(
+      `https://${wildcard == "australia" ? "" : `${wildcard}.`}hackclub.com/${req.query.path}`
+    )
+      .then((r) => r.text())
+      .catch(() =>
+        res.status(500).send("Encountered error serving profile page")
+      );
+    console.log(wildcard);
+    res.send(html);
+  }
+  
 };
